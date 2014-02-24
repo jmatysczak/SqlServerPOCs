f97516441c2933c1344e70bf5f3929ee9227eef2
jmat <jmat@alum.mit.edu>
2014-02-23 22:03:21 -0500

    Yeeah, stupid. Tired.

--- SortingAndPagingPerformance/CLRFunctions/PriorityQueuePager.cs
+++ SortingAndPagingPerformance/CLRFunctions/PriorityQueuePager.cs
@@ -66,7 +66,7 @@ namespace CLRFunctions {
       }
 
       public int Compare(string x, string y) {
-        return this.comparer.Compare(x, y) * -1;
+        return this.comparer.Compare(y, x);
       }
     }
   }
